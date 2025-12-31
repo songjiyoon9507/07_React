f9451c5674886eaede89132e978eae8c75a40209
songjiyoon9507 <songjiyoon9507@gmail.com>
2026-01-01 00:21:59 +0900

    DB 연결

---
+++ spring_react_login/backend/src/main/java/com/example/backend/domain/user/entity/UserEntity.java
@@ -0,0 +1,10 @@
+package com.example.backend.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+// 이 엔티티가 어떤 테이블과 연관돼있는지 나타냄
+public class UserEntity {
+
+}
